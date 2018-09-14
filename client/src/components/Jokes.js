a795995716fd4d24de364a864bdbef7c265bc928
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-14 13:22:29 -0400

    Jokes componenet compled

---
+++ client/src/components/Jokes.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+class Jokes extends Component {
+  state = {
+    jokes: []
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem("JWT") !== null) {
+      const JWT = localStorage.getItem("JWT");
+      axios
+        .get("http://localhost:3300/api/jokes", {
+          headers: { Authorization: JWT }
+        })
+        .then(response => {
+          this.setState({
+            jokes: response.data
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.map((element, index) => {
+            <li key={index}>
+              <div>
+                {element.setup}? {element.punchline}{" "}
+              </div>
+            </li>;
+          })}
+        </ul>
+        {this.state.jokes.length === 0 ? (
+          <h1>
+            Please <Link to="/login">Login</Link>
+          </h1>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
+
+export default Jokes;
